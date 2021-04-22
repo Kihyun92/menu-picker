fcc9bf98b16f92dcacef6f2c82720aede56b2c39
Kihyun92 <okkihyun1@gmail.com>
2021-04-23 00:48:52 +0900

    Practice python

--- src/menuPicker.py
+++ src/menuPicker.py
@@ -14,7 +14,7 @@ print(dinner)
 
 print('------')
 
-me = {"name": "기현", "age": 30, "sex": "male"}
+me = {"name": "기현", "age": 30, "sex": "male"} # 딕셔너리
 
 print(me.get("name"))
 
@@ -50,3 +50,43 @@ print('-----')
 
 for x in range(len(foods)):
   print(foods[x])
+
+inform = {"고향": "광주", "취미": "코딩", "좋아하는 음식": "피자"}
+for x, y in inform.items():
+  print(x)
+  print(y)
+
+print('-----')
+
+# 집합
+# set, 여러가지 원소의 묶음, 같은 원소가 존재할 수 없음
+
+foods = ["된장찌개", "피자", "제육볶음", "피자"]
+foods_set = set(foods) # 안에들어있는 원소의 순서를 보장하지 않음
+
+print(foods)
+print(foods_set)
+
+print('-----')
+
+menu1 = set(["된장찌개", "피자", "제육볶음"])
+menu2 = set(["피자", "치킨", "짜장면"])
+
+# 합집합
+menu3 = menu1 | menu2
+print(menu3)
+
+# 교집합
+menu3 = menu1 & menu2
+print(menu3)
+
+# 차집합
+menu3 = menu1 - menu2
+print(menu3)
+
+print('-----')
+
+# if
+food = random.choice(["피자", "치킨", "짜장면"])
+
+
